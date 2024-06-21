69099eff376f61e77634b460054f85aecf222c7c
NGUYEN LUAN <nguyenluan0101020@gmail.com>
2024-06-21 11:03:53 +0700

    add check current accesstoken of admin for Home Page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import HeaderPage from "./header_page";
 import FooterPage from "./footer_page";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+    const navigator = useNavigate();
+    const accessToken = localStorage.getItem("accessToken");
+
+    useEffect(() => {
+        if (accessToken == null) {
+            navigator("/");
+        }
+    }, [accessToken, navigator])
 
     return (
         <>
