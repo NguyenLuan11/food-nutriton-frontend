293c122fd5588d346fa2fd97411d679b798bffce
NguyenLuan11 <nguyenluan0101020@gmail.com>
2024-07-03 18:54:00 +0700

    add count all items

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -18,7 +18,69 @@ const HomePage = () => {
         <>
         <HeaderPage />
             <div className="container">
-                
+
+                <div className="row row-cols-5">
+                    <div className="col">
+                        <div className="w-100 shadow bg-success rounded-4 m-3 p-3">
+                            <div className="d-flex flex-row justify-content-between">
+                                <h3>
+                                    <input disabled className="form-control-plaintext fw-bold fs-5 text-light" type="text" 
+                                        name="" id="" value="Users"/>
+                                </h3>
+                                <h5 className="text-light mt-3">5</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="w-100 shadow bg-warning rounded-4 m-3 p-3">
+                            <div className="d-flex flex-row justify-content-between">
+                                <h3>
+                                    <input disabled className="form-control-plaintext fw-bold fs-5" type="text" 
+                                            name="" id="" value="Foods"/>
+                                </h3>
+                                <h5 className="mt-3">2</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="w-100 shadow bg-danger rounded-4 m-3 p-3">
+                            <div className="d-flex flex-row justify-content-between">
+                                <h3>
+                                    <input disabled className="form-control-plaintext fw-bold fs-5 text-light" type="text" 
+                                            name="" id="" value="Nutrients"/>
+                                </h3>
+                                <h5 className="text-light mt-3">1</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="w-100 shadow bg-info rounded-4 m-3 p-3">
+                            <div className="d-flex flex-row justify-content-between">
+                                <h3>
+                                    <input disabled className="form-control-plaintext fw-bold fs-5" type="text" 
+                                            name="" id="" value="Categories"/>
+                                </h3>
+                                <h5 className="mt-3">2</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="w-100 shadow bg-secondary rounded-4 m-3 p-3">
+                            <div className="d-flex flex-row justify-content-between">
+                                <h3>
+                                    <input disabled className="form-control-plaintext fw-bold fs-5 text-light" type="text" 
+                                            name="" id="" value="Articles"/>
+                                </h3>
+                                <h5 className="text-light mt-3">1</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         <FooterPage />
         </>
